710f9d249fbe229f8db251d4c17b71675b810a73
christopherhanke <christopherhanke@googlemail.com>
2024-05-30 17:56:56 +0200

    adding cell class. init and draw method. draw window test.

---
+++ graphics.py
@@ -0,0 +1,68 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, heigth):
+        self.__root = Tk()
+        self.__canvas = Canvas(height=heigth, width=width, bg="white")
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__visible = False
+        self.__root.title("Mazeing")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.__visible = True
+        while self.__visible:
+            self.redraw()
+    
+    def close(self):
+        self.__visible = False
+    
+    def draw_line(self, line, color):
+        line.draw(self.__canvas, color)
+
+
+class Point():
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, color):
+        canvas.create_line(
+            self.point1.x, self.point1.y,
+            self.point2.x, self.point2.y,
+            fill=color,
+            width=2
+        )
+
+
+class Cell():
+    def __init__(self, x1, y1, x2, y2, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = window
+    
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black")
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black")
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black")
--- main.py
+++ main.py
@@ -1,60 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window():
-    def __init__(self, width, heigth):
-        self.__root = Tk()
-        self.__canvas = Canvas(height=heigth, width=width, bg="black")
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__visible = False
-        self.__root.title("Mazeing")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-    
-    def wait_for_close(self):
-        self.__visible = True
-        while self.__visible:
-            self.redraw()
-    
-    def close(self):
-        self.__visible = False
-    
-    def draw_line(self, line, color):
-        line.draw(self.__canvas, color)
-
-
-class Point():
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-class Line():
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-    
-    def draw(self, canvas, color):
-        canvas.create_line(
-            self.point1.x, self.point1.y,
-            self.point2.x, self.point2.y,
-            fill=color,
-            width=2
-        )
-
+from graphics import Window, Cell
 
 
 def main():
     win = Window(800, 600)
-    x = Point()
-    y = Point(100, 300)
-    z = Point(200, 200)
-    a = Point(400, 400)
-    
-    win.draw_line(Line(x, y), "white")
-    win.draw_line(Line(a, z), "red")
-    win.draw_line(Line(a, y), "green")
+
+    Cell(0,0 , 100,100, win).draw()
+    Cell(10,10 , 50,50, win).draw()
 
     win.wait_for_close()
 
